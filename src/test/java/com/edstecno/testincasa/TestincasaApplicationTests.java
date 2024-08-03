4bc7114694c01d9b849e88a6ff80f0829bc29a4b
edarcega <edimar.eds@gmail.com>
2024-08-03 12:47:28 -0300

    Projeto spring criado

---
+++ src/test/java/com/edstecno/testincasa/TestincasaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edstecno.testincasa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestincasaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
