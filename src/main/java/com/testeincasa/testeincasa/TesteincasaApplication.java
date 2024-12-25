08074cf77255099c94fd536e8e6704805336f3ca
edimar <edimar.eds@gmail.com>
2024-12-25 19:52:08 -0300

    Projeto criado

---
+++ src/main/java/com/testeincasa/testeincasa/TesteincasaApplication.java
@@ -0,0 +1,13 @@
+package com.testeincasa.testeincasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteincasaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteincasaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testeincasa/testeincasa/TesteincasaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testeincasa.testeincasa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteincasaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
