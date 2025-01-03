72e97ae8195c1a528f9aeec7bf6f5cf15b7e4d31
edimar <edimar.eds@gmail.com>
2025-01-03 11:27:46 -0300

    Criando consultas simples com query methods

--- src/main/java/com/testeincasa/testeincasa/services/PostService.java
+++ src/main/java/com/testeincasa/testeincasa/services/PostService.java
@@ -6,6 +6,7 @@ import com.testeincasa.testeincasa.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -18,6 +19,10 @@ public class PostService {
     public Post findById(String id) {
         Optional<Post> post = postRepository.findById(id);
         return post.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public List<Post> findByTitle(String text) {
+        return postRepository.findByTitleContainingIgnoreCase(text);
     }
 
 }
